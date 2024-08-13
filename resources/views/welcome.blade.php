4a15a1c15547cb1ab315842b85d94060fa288f08
NjauSamuel <njaus602@gmail.com>
2024-08-13 10:52:05 +0300

    Got started on Mpesa Integration. The UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,44 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
 </head>
-<body>
-    <div class="min-h-[100vh] flex justify-center items-center">
-        <h1 class="text-3xl font-bold underline">
-          Hello World!
-        </h1>
+<body class="mx-auto mt-10 max-w-2xl bg-gradient-to-r from-indigo-200 from-10% via-sky-200 via-30% to-emerald-200 to-90% text-slate-700">
+
+    <div class="rounded-md border border-slate-300 bg-white p-4 shadow-sm">
+      <h2 class="text-xl font-semibold text-gray-800 mb-4">Obtain Access Token</h2>
+
+      <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md">Request Access Token!</button>
+    </div>
+
+    <div class="rounded-md border border-slate-300 bg-white p-4 shadow-sm mt-5">
+      <h2 class="text-xl font-semibold text-gray-800 mb-4">Register URL!</h2>
+
+      <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md">Register URL's</button>
+    </div>
+
+    <div class="rounded-md border border-slate-300 bg-white p-4 shadow-sm mt-5">
+      <h2 class="text-xl font-semibold text-gray-800 mb-4">Simulate Transactions</h2>
+
+      <form action="" class="space-y-4">
+        @csrf
+        <div class="flex flex-col">
+          <label for="amount" class="mb-1 text-sm font-medium text-gray-700">Amount:</label>
+          <input type="number" name="amount" id="amount" class="w-full px-3 py-1 border border-gray-300 rounded-md">
+        </div>
+
+        <div class="flex flex-col">
+          <label for="account" class="mb-1 text-sm font-medium text-gray-700">Account:</label>
+          <input type="text" name="account" id="account" class="w-full px-3 py-1 border border-gray-300 rounded-md">
+        </div>
+
+        <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md">Simulate Transactions</button>
+      </form>
+
+    </div>
+
+    <div class="rounded-md border border-slate-300 bg-white p-4 shadow-sm mt-5">
+      <h2 class="text-xl font-semibold text-gray-800 mb-4">Register URL!</h2>
+
+      <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md">Register URL's</button>
     </div>
 </body>
 </html>
