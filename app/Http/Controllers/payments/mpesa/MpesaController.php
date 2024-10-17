ea50b479f9342d39cf47badf74d44a99520e8dde
NjauSamuel <njaus602@gmail.com>
2024-10-17 22:41:35 +0300

    The Register URL not working was a result of the Mpesa Side, they were using v2 api url.

--- app/Http/Controllers/payments/mpesa/MpesaController.php
+++ app/Http/Controllers/payments/mpesa/MpesaController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 
 class MpesaController extends Controller
 {
-    public function getAccessToken(){
+    public function getAccessToken()
+    {
         $url = 'https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
 
         $curl = curl_init($url);
@@ -17,7 +18,7 @@ class MpesaController extends Controller
                 CURLOPT_HTTPHEADER => ['Content-Type: application/json; charset=utf8'],
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_HEADER => false,
-                CURLOPT_USERPWD => env('MPESA_CONSUMER_KEY').':'.env('MPESA_CONSUMER_SECRET'),
+                CURLOPT_USERPWD => env('MPESA_CONSUMER_KEY') . ':' . env('MPESA_CONSUMER_SECRET'),
                 CURLOPT_SSL_VERIFYPEER => false, // Disables SSL certificate verification
                 CURLOPT_SSL_VERIFYHOST => false, // Disables SSL host verification
             )
@@ -37,7 +38,7 @@ class MpesaController extends Controller
         if (empty($response)) {
             dd("Empty response received from the API.");
         }
-        
+
         $responseData = json_decode($response, true); // Decode the JSON response into an array
 
         //return view('welcome', ['responseData' => $responseData]);
@@ -45,17 +46,18 @@ class MpesaController extends Controller
         return $responseData['access_token'];
     }
 
-    public function makeHttp($url, $body){
+    public function makeHttp($url, $body)
+    {
 
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization:Bearer '. $this->getAccessToken()));
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); 
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization:Bearer ' . $this->getAccessToken()));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, \json_encode($body));
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false );
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-       
+
         $curl_response = curl_exec($curl);
         \curl_close($curl);
 
@@ -66,17 +68,18 @@ class MpesaController extends Controller
     *Register URL
     */
 
-    public function registerURLS(){   
+    public function registerURLS()
+    {
 
         $url = env('MPESA_ENV') === '0'
-        ? 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl' 
-        : 'https://api.safaricom.co.ke/mpesa/c2b/v1/registerurl';
+            ? 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl'
+            : 'https://api.safaricom.co.ke/mpesa/c2b/v2/registerurl';
 
 
-        $body = array( 
+        $body = array(
             'ShortCode' => env('MPESA_SHORTCODE'),
             'ResponseType' => 'Completed',
-            'ConfirmationURL' => env('MPESA_PRODUCTION_URL') . '/api/confirmation', 
+            'ConfirmationURL' => env('MPESA_PRODUCTION_URL') . '/api/confirmation',
             'ValidationURL' => env('MPESA_PRODUCTION_URL') . '/api/validation'
         );
 
